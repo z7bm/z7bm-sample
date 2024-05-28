4cd121df1a9640e3e0d278cd3754a4156ea65b7b
hz <harry.zhurov@gmail.com>
2024-05-28 14:16:28 +0700

    add main fun for cam program

--- src/sw/apu/cam/main.cpp
+++ src/sw/apu/cam/main.cpp
@@ -1,4 +1,8 @@
 
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
 #include <ps7mmrs.h>
 #include "z7int.h"
 #include "z7qspi.h"
@@ -11,33 +15,87 @@ void swi_isr_handler();
 void gpio_isr_handler();
 void default_isr_handler();
 
-uint8_t Buf[1024];
-uint8_t bbb = 0x34;
-uint8_t aaa = 0x56;
-uint8_t ccc = 0x78;
 Uart uart1(UART1_ADDR);
 
-uint32_t slonick(uint32_t x)
-{
-    volatile static uint32_t Data[3000] = { 1, 2, 3, 4  };
-    
-    return Data[x]+ bbb; // +aaa + ccc;
-}
+int aaa = 11;
 
-__attribute__((section(".kot"))) __attribute__ ((aligned (4))) 
-uint8_t slonick2 = 5;
+//------------------------------------------------------------------------------
+const int TX_BUF_SIZE = 256;
+
+char print_buf[TX_BUF_SIZE];
+int print(const char *format, ...)
+{
+    va_list args;                                      //
+    va_start(args, format);                            //
+    uint16_t size = vsprintf(print_buf, format, args); //
+    va_end(args);                                      //
+                                                       //
+    uart1.send(print_buf);
+    //TxBuf.write(print_buf, size);                      // put formatted data to port buffer
+                                                       //
+    return size;
+}
+//------------------------------------------------------------------------------
+
+
 
 //------------------------------------------------------------------------------
 int main() 
 { 
-    for(uint32_t i = 0; i < sizeof(Buf); ++i)
+    //----------------------------------------------------------------
+    //
+    //    Setup interrupts
+    //
+
+    //------------------------------------------------------
+    //
+    //    Initialize interrupt handlers table
+    //
+    for(uint32_t i = 0; i < PS7_MAX_IRQ_ID; ++i)
     {
-        Buf[i] = slonick(i) + slonick2;
+        ps7_register_isr_handler(&default_isr_handler, i);
     }
+
+    ps7_register_isr_handler(&swi_isr_handler,  PS7IRQ_ID_SW7);
+    ps7_register_isr_handler(&gpio_isr_handler, PS7IRQ_ID_GPIO);
+
+    //------------------------------------------------------
+    //
+    //    Setup GPIO interrupt
+    //
+    gpio_clr_int_sts(PIN_INT);
+    gpio_int_pol(PIN_INT, GPIO_INT_POL_HIGH_RISE);
+    gpio_int_en(PIN_INT);
+
+    gic_int_enable(PS7IRQ_ID_UART1);
+    gic_set_target(PS7IRQ_ID_UART1, GIC_CPU0);
+
+    gic_set_target(PS7IRQ_ID_GPIO, 1ul << GIC_CPU0);
+    gic_set_config(PS7IRQ_ID_GPIO, GIC_EDGE_SINGLE);
+    gic_int_enable(PS7IRQ_ID_GPIO);
+
+    sbpa(GIC_ICCPMR, 0xff);
+    gic_set_priority(PS7IRQ_ID_SW7, 16);
+
+    sbpa(GIC_ICDDCR, 0x1);  // enable GIC Distributor
+    sbpa(GIC_ICCICR, 0x1);  // enable interruptes for CPU interfaces
+
+    //------------------------------------------------------
+    //
+    //    Initialize peripherals
+    //
+    uart1.init();
+
+    enable_interrupts();
+
+    print("cam: program start!\n");
+
+    print("aaa: %d\n", aaa);
 
     for(;;)
     {
-        
+        wrpa(GPIO_MASK_DATA_0_LSW_REG, (~(1ul << 10) << 16) | (1ul << 10) );  // JE2 on
+        wrpa(GPIO_MASK_DATA_0_LSW_REG, (~(1ul << 10) << 16) | 0 );            // JE2 off
     }
 }
 //------------------------------------------------------------------------------
