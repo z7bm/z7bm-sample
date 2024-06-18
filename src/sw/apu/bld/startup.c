6c85250fa281aed71f7b8fd630fc14605bf411e4
hz <harry.zhurov@gmail.com>
2024-06-18 20:15:40 +0700

    update titles

--- src/sw/apu/bld/startup.c
+++ src/sw/apu/bld/startup.c
@@ -1,38 +1,35 @@
-//******************************************************************************
-//*
-//*     Project:   Any
-//*
-//*     File:      Cortex-A startup file
-//*
-//*     Version 2.0
-//*
-//*     Copyright (c) 2016, emb-lib Project Team
-//*
-//*     Permission is hereby granted, free of charge, to any person
-//*     obtaining  a copy of this software and associated documentation
-//*     files (the "Software"), to deal in the Software without restriction,
-//*     including without limitation the rights to use, copy, modify, merge,
-//*     publish, distribute, sublicense, and/or sell copies of the Software,
-//*     and to permit persons to whom the Software is furnished to do so,
-//*     subject to the following conditions:
-//*
-//*     The above copyright notice and this permission notice shall be included
-//*     in all copies or substantial portions of the Software.
-//*
-//*     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-//*     EXPRESS  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-//*     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-//*     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-//*     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-//*     TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
-//*     THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//*
+//------------------------------------------------------------------------------
+//
+//    C/C++ Startup Source
+//
+//    Permission is hereby granted, free of charge, to any person
+//    obtaining  a copy of this software and associated documentation
+//    files (the "Software"), to deal in the Software without restriction,
+//    including without limitation the rights to use, copy, modify, merge,
+//    publish, distribute, sublicense, and/or sell copies of the Software,
+//    and to permit persons to whom the Software is furnished to do so,
+//    subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included
+//    in all copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//    EXPRESS  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+//    THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//    Copyright (c) 2017-2024, Zynq-7000 Bare-metal Project
+//    -----------------------------------------------------
+//    Project sources: https://github.com/z7bm
+//
 //------------------------------------------------------------------------------
 
 #include <string.h>
 #include "ps7_init.h"
 #include "xil_cache.h"
-
      
 //------------------------------------------------------------------------------
 
